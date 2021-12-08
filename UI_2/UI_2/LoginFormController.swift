b2eaf27f4e056e8b6b26e4855247a91df8c5cce7
Nikita <nikitaaalimpiev@gmail.com>
2021-12-08 23:13:31 +0500

    Revert "Add session (token, userid)"
    This reverts commit 92f5b21bca33befee1f81b99bc55b0108c9c0d1a.

--- UI_2/UI_2/LoginFormController.swift
+++ UI_2/UI_2/LoginFormController.swift
@@ -72,12 +72,7 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var loginInput: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBAction func loginButtomPressed(_ sender: Any) {
-    
-        guard let token = loginInput.text
-        else {return}
-        Session.shared.token = token
-        
-        // Получаем текст логина
+    // Получаем текст логина
     let login = loginInput.text!
     // Получаем текст-пароль
     let password = passwordInput.text!
