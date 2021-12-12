2826f13adcf14ba96a0da83546d8f46f27a6d80c
Nikita <nikitaaalimpiev@gmail.com>
2021-12-12 16:34:14 +0500

    Add VkAuth
    Errors:
    
    1. To log in through VK, you need input login/password admin/123456/.
    
    2. After VK login you got a misstake "Thread 1: "Completion handler passed to -[UI_2.VkAuthViewController webView:decidePolicyForNavigationResponse:decisionHandler:] was not called""

--- UI_2/UI_2/Session.swift
+++ UI_2/UI_2/Session.swift
@@ -9,6 +9,6 @@ import Foundation
 final class Session {
      private init() {}
      static let shared = Session()
-     var token: String = ""
-     var userId: Int = 0
+     var token = ""
+     var userId = ""
  }
