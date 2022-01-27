3591456eb72a7fd57b5452ad9beb1a14d8582c1c
Nikita <nikitaaalimpiev@gmail.com>
2022-01-27 23:20:08 +0500

    Add FeedBoard

---
+++ UI_2/UI_2/Feed/FeedBoard.swift
@@ -0,0 +1,13 @@
+//
+//  FeedBoard.swift
+//  UI_2
+//
+//  Created by Никита Алимпиев on 27.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class FeedBoard: UITableViewController{
+    
+}
---
+++ UI_2/UI_2/Feed/FeedCell.swift
@@ -0,0 +1,21 @@
+//
+//  FeedCell.swift
+//  UI_2
+//
+//  Created by Никита Алимпиев on 27.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class FeedCell: UITableViewCell{
+    @IBOutlet weak var lectureImage: UIImageView!
+    @IBOutlet weak var lectureName: UILabel!
+    @IBOutlet weak var lectureDescription: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var authorDescription: UILabel!
+    @IBOutlet weak var lectureLike: UIImageView!
+    @IBOutlet weak var lectureComment: UIImageView!
+    
+}
